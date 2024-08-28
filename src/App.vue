5e9d3acb8927959b356ed2715df34bb9fa01d408
William Bullock <63573688+WEB-Track@users.noreply.github.com>
2024-08-27 20:16:21 -0400

    first commit
    Starter VUE Template

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo layout-logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue layout-logo" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss">
+  @import './assets/scss/main.scss' 
+  </style>
