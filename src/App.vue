ae8545b31eef73eb120ae1f96a9e70a6e82a7efd
William Bullock <63573688+WEB-Track@users.noreply.github.com>
2024-08-29 16:44:04 -0400

    fixed tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <div class="grid grid-cols-4 pb-5">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo layout-logo" alt="Vite logo"/>
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue layout-logo" alt="Vue logo" />
+    </a>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo layout-logo" alt="Vite logo" />
     </a>
@@ -11,9 +17,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue layout-logo" alt="Vue logo" />
     </a>
   </div>
-    <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style lang="scss">
-  @use './assets/scss/main.scss' 
+  @use './assets/scss/style.scss' 
 </style>
